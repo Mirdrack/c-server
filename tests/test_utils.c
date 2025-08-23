dd26dc87ae095b9a705c0d72108d21eac6a06e8b
Clemente Estrada <mirdrack@gmail.com>
2025-08-23 21:18:00 +0200

    feat: add test scaffolding include the folder and file structure to have a test suite and use check library

---
+++ tests/test_utils.c
@@ -0,0 +1,58 @@
+#include <check.h>
+#include "../include/utils.h"
+
+START_TEST(test_simple_extension) {
+    ck_assert_str_eq(get_file_extention("file.txt"), "txt");
+}
+END_TEST
+
+START_TEST(test_multiple_dots) {
+    ck_assert_str_eq(get_file_extention("archive.tar.gz"), "gz");
+    ck_assert_str_eq(get_file_extention("a.b.c.d"), "d");
+}
+END_TEST
+
+START_TEST(test_no_extension) {
+    ck_assert_str_eq(get_file_extention("file"), "");
+}
+END_TEST
+
+START_TEST(test_hidden_file) {
+    ck_assert_str_eq(get_file_extention(".hiddenfile"), "");
+}
+END_TEST
+
+START_TEST(test_trailing_dot) {
+    ck_assert_str_eq(get_file_extention("file."), "");
+}
+END_TEST
+
+START_TEST(test_only_dots) {
+    ck_assert_str_eq(get_file_extention(".."), "");
+}
+END_TEST
+
+START_TEST(test_empty_string) {
+    ck_assert_str_eq(get_file_extention(""), "");
+}
+END_TEST
+
+Suite* utils_suite(void) {
+    Suite *s;
+    TCase *tc_core;
+
+    s = suite_create("FileUtils");
+
+    tc_core = tcase_create("Core");
+    tcase_add_test(tc_core, test_simple_extension);
+    tcase_add_test(tc_core, test_multiple_dots);
+    tcase_add_test(tc_core, test_no_extension);
+    tcase_add_test(tc_core, test_hidden_file);
+    tcase_add_test(tc_core, test_trailing_dot);
+    tcase_add_test(tc_core, test_only_dots);
+    tcase_add_test(tc_core, test_empty_string);
+
+    suite_add_tcase(s, tc_core);
+    return s;
+}
+
