487e469d36d2394228796c8f7bf3afe3ad4c982b
Clemente Estrada <mirdrack@gmail.com>
2025-08-24 18:10:29 +0200

    chore: apply format to the existing code

--- include/server.h
+++ include/server.h
@@ -4,9 +4,7 @@
 #include <stddef.h>
 
 void *handle_client(void *arg);
-void build_http_response(const char * filename,
-    const char *file_ext,
-    char *reponse,
-    size_t *response_len);
+void build_http_response(const char *filename, const char *file_ext, char *reponse,
+                         size_t *response_len);
 
 #endif // SERVER_H
