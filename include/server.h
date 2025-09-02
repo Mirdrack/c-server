4e4581abd7b9a26c180dbaba64ae14fd53a4db28
Clemente Estrada <mirdrack@gmail.com>
2025-09-02 20:39:13 +0200

    fix: update names in the signature to match defintion

--- include/server.h
+++ include/server.h
@@ -4,7 +4,7 @@
 #include <stddef.h>
 
 void *handle_client(void *arg);
-void build_http_response(const char *filename, const char *file_ext, char *reponse,
+void build_http_response(const char *file_name, const char *file_ext, char *response,
                          size_t *response_len);
 
 #endif // SERVER_H
